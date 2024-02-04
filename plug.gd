b0005ac5bf18b244e3b21c16656a113ea70c8a4d
florianvazelle <ponythugflorian@gmail.com>
2024-02-04 10:22:21 +0100

    ci: rework renovate config to use a branch name for commit

--- plug.gd
+++ plug.gd
@@ -4,7 +4,13 @@ extends "res://addons/gd-plug/plug.gd"
 func _plugging():
 	plug(
 		"godot-extended-libraries/godot-debug-menu",
-		{"commit": "9d36ea23661d095198ff7fcfff2715172f73c983"}
+		{"commit": "9d36ea23661d095198ff7fcfff2715172f73c983", "renovate-branch": "master"}
 	)
-	plug("KoBeWi/Godot-Universal-Fade", {"commit": "f091514bba652880f81c5bc8809e0ee4498988ea"})
-	plug("nisovin/godot-coi-serviceworker", {"commit": "de1be2989eda4c7d77a08b8c56cd94c769181c4e"})
+	plug(
+		"KoBeWi/Godot-Universal-Fade",
+		{"commit": "f091514bba652880f81c5bc8809e0ee4498988ea", "renovate-branch": "master"}
+	)
+	plug(
+		"nisovin/godot-coi-serviceworker",
+		{"commit": "de1be2989eda4c7d77a08b8c56cd94c769181c4e", "renovate-branch": "main"}
+	)
