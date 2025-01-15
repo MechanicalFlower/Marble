e31aadb5db5f5b7fbdb254c88d1cdd9c4f56bc3d
florianvazelle <ponythugflorian@gmail.com>
2025-01-15 22:00:03 +0100

    feat: add readme instruction & ci job

--- flake.nix
+++ flake.nix
@@ -3,16 +3,13 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
-    flake-utils.url = "github:numtide/flake-utils";
-    godot.url = "path:/home/phlowrient/projects/by-lang/nix/godot-nix";
+    godot.url = "github:florianvazelle/godot-overlay/rework";
   };
 
   outputs = {
     self,
     nixpkgs,
-    flake-utils,
     godot,
-    # nixgl,
     ...
   }: let
     system = "x86_64-linux";
