70a7aee18e70268f1d120f9270e176f56ea052ec
florianvazelle <ponythugflorian@gmail.com>
2025-01-17 09:06:35 +0100

    ci: adapt renovate managers

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
-    godot.url = "github:florianvazelle/godot-overlay/rework";
+    godot.url = "github:florianvazelle/godot-overlay";
     godot.inputs.nixpkgs.follows = "nixpkgs";
   };
 
